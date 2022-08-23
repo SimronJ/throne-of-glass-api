8dd112bb0e424721b712c1ea8c7ca777edca2cc7
SimronJ <13simran37@gmail.com>
2022-08-23 12:56:38 -0400

    MY First REST API deploy

---
+++ pages/api/characters/[id].ts
@@ -0,0 +1,22 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+
+const handler = async(req: NextApiRequest, res: NextApiResponse) =>{
+    if(req.method !== 'GET') return res.status(400);
+
+    const id = req.query.id;
+    const idStr = Array.isArray(id) ? id[0] : id || '';
+    const idNum = parseInt(idStr);
+
+    if(isNaN(idNum)) return res.status(400).json({err : "Please enter a valid number for character id"});
+
+    const character = await prisma?.tog_character.findUnique({
+        where : {
+            id : idNum
+        }
+    });
+
+    res.json(character);
+}
+
+export default handler;
